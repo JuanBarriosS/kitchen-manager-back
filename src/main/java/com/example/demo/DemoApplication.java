5ae3807bf41d688702e09a8f071386e7df5edb61
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-04-29 20:07:44 -0500

    Add PostConstruct to set default time zone
    Set default time zone to America/Bogota on application startup.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	@PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
