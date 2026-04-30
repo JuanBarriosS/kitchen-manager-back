6bd38ad6b1d81bacab6269244e30a7c363ec1ee2
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-04-29 20:12:17 -0500

    Fix missing semicolon in DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.annotation.PostConstruct;
-import java.util.TimeZone
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DemoApplication {
