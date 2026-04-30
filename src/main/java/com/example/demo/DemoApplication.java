769345de15b35b070207acf61892e138f6602f4c
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-04-29 20:16:12 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class DemoApplication {
-	
-	@PostConstruct
-    void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
